4ad5bbfb700238a925ac9945d7903a8cfffe5e05
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2020-09-28 05:58:15 +0530

    Updated Bot script for any path

--- Bot001.ino
+++ Bot001.ino
@@ -4,13 +4,13 @@ int s1=3;
  int m2=6;
  int m3=7;
  int m4=8;
-
+ int c=0;
  void setup()
  {
   pinMode(s1,INPUT);
-  delay(500);
+delay(500);
   pinMode(s2,INPUT);
-  delay(500);
+delay(500);
   pinMode(m1,OUTPUT);
   pinMode(m2,OUTPUT);
   pinMode(m3,OUTPUT);
@@ -18,31 +18,38 @@ int s1=3;
   }
   void loop()
   {
-    if(digitalRead(s1)==LOW && digitalRead(s2)==LOW)
+    if(c<=1 && digitalRead(s1)==LOW && digitalRead(s2)==LOW)
     {digitalWrite(m1,HIGH);  
     digitalWrite(m2,LOW);  
     digitalWrite(m3,HIGH);  
-    digitalWrite(m4,LOW); }
-
-     if(digitalRead(s1)==LOW && digitalRead(s2)==HIGH)
-    {digitalWrite(m1,LOW);  
-    digitalWrite(m2,HIGH);  
-    digitalWrite(m3,HIGH);  
-    digitalWrite(m4,LOW);
-   }  
-     
-     if(digitalRead(s1)==HIGH && digitalRead(s2)==LOW)
-    {digitalWrite(m1,HIGH);  
-    digitalWrite(m2,LOW);  
-    digitalWrite(m3,LOW);  
-    digitalWrite(m4,HIGH);
-    } 
-    
-     if(digitalRead(s1)==HIGH && digitalRead(s2)==HIGH)
-    {digitalWrite(m1,HIGH);  
-    digitalWrite(m2,LOW);  
-    digitalWrite(m3,LOW);  
-    digitalWrite(m4,HIGH);
+    digitalWrite(m4,LOW); 
     }
-      
+
+     if(c<=1 && digitalRead(s1)==LOW && digitalRead(s2)==HIGH)
+    {digitalWrite(m1,HIGH);  
+    digitalWrite(m2,LOW);  
+    digitalWrite(m3,HIGH);  
+    digitalWrite(m4,LOW); 
+    c++;}  
+     
+     if(c<=1 && digitalRead(s1)==HIGH && digitalRead(s2)==LOW)
+    {digitalWrite(m1,HIGH);  
+    digitalWrite(m2,LOW);  
+    digitalWrite(m3,HIGH);  
+    digitalWrite(m4,LOW); 
+    c++;}
+    
+     if(c<=1 && digitalRead(s1)==HIGH && digitalRead(s2)==HIGH)
+    {digitalWrite(m1,HIGH);  
+    digitalWrite(m2,LOW);  
+    digitalWrite(m3,HIGH);  
+    digitalWrite(m4,LOW); 
+    c++;}
+    
+    else if(c>1)
+    {digitalWrite(m1,LOW);  
+    digitalWrite(m2,LOW);  
+    digitalWrite(m3,LOW);  
+    digitalWrite(m4,LOW); } 
+    
     }
