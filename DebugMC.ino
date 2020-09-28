469a65496978e5be31b185759c0275b3951c97d2
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2020-09-28 05:55:56 +0530

    Create DebugMC.ino
    This script is used for debugging the micro-controller

---
+++ DebugMC.ino
@@ -0,0 +1,18 @@
+Int s=3;
+Int m=4;
+void setup()
+{
+pinMode(s,INPUT);
+pinMode (m,OUTPUT);
+}
+
+void loop()
+{
+
+if(digitalRead(s)==LOW)
+{digitalWrite(m)==HIGH)}
+
+if(digitalRead(s)==HIGH))
+{digitalWrite(m)==LOW)}
+
+}
